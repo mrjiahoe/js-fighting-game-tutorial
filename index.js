21d4774de2b76f757ebbdc36ef089a864050d304
mrjiahoe <mrjiahoe@hotmail.com>
2022-04-10 16:49:47 +0800

    player jump animation

--- index.js
+++ index.js
@@ -55,6 +55,14 @@ const player = new Fighter({
 			imageSrc: "./img/samuraiMack/Run.png",
 			framesMax: 8,
 		},
+		jump: {
+			imageSrc: "./img/samuraiMack/Jump.png",
+			framesMax: 2,
+		},
+		fall: {
+			imageSrc: "./img/samuraiMack/Fall.png",
+			framesMax: 2,
+		},
 	},
 });
 
@@ -108,14 +116,21 @@ function animate() {
 
 	// player movement
 	player.velocity.x = 0;
-	player.image = player.sprites.idle.image;
-
 	if (keys.a.pressed && player.lastKey === "a") {
 		player.velocity.x = -5;
-		player.image = player.sprites.run.image;
+		player.switchSprite("run");
 	} else if (keys.d.pressed && player.lastKey === "d") {
 		player.velocity.x = 5;
-		player.image = player.sprites.run.image;
+		player.switchSprite("run");
+	} else {
+		player.switchSprite("idle");
+	}
+
+	// player jumping
+	if (player.velocity.y < 0) {
+		player.switchSprite("jump");
+	} else if (player.velocity.y > 0) {
+		player.switchSprite("fall");
 	}
 
 	// enemy movement
