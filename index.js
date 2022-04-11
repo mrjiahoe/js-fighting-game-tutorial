e2ec23f2aaac7220c0cb89017f16793d146dca94
mrjiahoe <mrjiahoe@hotmail.com>
2022-04-11 13:41:13 +0800

    interface design and animation

--- index.js
+++ index.js
@@ -176,6 +176,8 @@ function animate() {
 	c.fillRect(0, 0, canvas.width, canvas.height);
 	background.update();
 	shop.update();
+	c.fillStyle = "rgba(255,255,255,0.15)";
+	c.fillRect(0, 0, canvas.width, canvas.height);
 	player.update();
 	enemy.update();
 
@@ -229,7 +231,9 @@ function animate() {
 	) {
 		enemy.takeHit();
 		player.isAttacking = false;
-		document.querySelector("#enemyHealth").style.width = enemy.health + "%";
+		gsap.to("#enemyHealth", {
+			width: enemy.health + "%",
+		});
 	}
 
 	// if player misses
@@ -248,7 +252,9 @@ function animate() {
 	) {
 		player.takeHit();
 		enemy.isAttacking = false;
-		document.querySelector("#playerHealth").style.width = player.health + "%";
+		gsap.to("#playerHealth", {
+			width: player.health + "%",
+		});
 	}
 
 	// if enemy misses
